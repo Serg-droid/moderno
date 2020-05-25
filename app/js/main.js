a81abd5f7d3a31341a72560f5cdb15d7c7142c50
Сергей Худяков <sergei.hudakov@gmail.com>
2020-05-26 02:24:26 +0300

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function(){
 
+    const mixer = mixitup('.products__inner-box');
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: "12px",
+    });
+
+    $('.product-slider__inner').slick({
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+    });
 });
